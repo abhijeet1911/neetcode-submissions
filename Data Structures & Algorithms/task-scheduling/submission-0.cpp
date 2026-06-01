4db03267229c0e64d0242bb0000e3ce0644a6dfa
abhijeet1911 <bairagiabhijeet904@gmail.com>
2026-06-01 16:39:58 +0530

    Add: task-scheduling - submission-0

---
+++ Data Structures & Algorithms/task-scheduling/submission-0.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        priority_queue<int>pq;
+        int time=0;
+        vector<int>mp(26,0);
+        for( char task:tasks){
+        mp[task-'A']++;
+    }
+    for(int i=0;i<mp.size();i++){
+        if(mp[i]>0)
+        pq.push(mp[i]);
+
+    }
+    while(!pq.empty()){
+        vector<int>temp;
+        for(int i=1;i<=n+1;i++){
+            if(!pq.empty()){
+             int freq=pq.top();
+             pq.pop();
+             freq--;
+             temp.push_back(freq);
+
+            }
+        }
+        for(int &p:temp){
+            if(p>0)
+            pq.push(p);
+
+        }
+        
+        if(pq.empty()){
+            time+=temp.size();
+        }
+        else{
+            time+=n+1;
+        }
+
+    }
+    
+    
+    
+    return time;
+    
+    
+    
+    }
+};
