eaca4e855d55753abd9021a63d18cb0f70ce508c
abhijeet1911 <bairagiabhijeet904@gmail.com>
2026-06-06 13:02:01 +0530

    Add: partition-labels - submission-0

---
+++ Data Structures & Algorithms/partition-labels/submission-0.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        vector<int>result;
+        int n=s.size();
+        vector<int>mp(26,-1);
+        for(int i=0;i<n;i++){
+            mp[s[i]-'a']=i;
+        }
+        int i=0;
+        while(i<n){
+       int end=mp[s[i]-'a'];
+       int j=i;
+       while(j<end){
+         end=max(end,mp[s[j]-'a']);
+         j++;
+       }
+       result.push_back(j-i+1);
+       i=end+1;
+        }
+        return result;
+    }
+};
